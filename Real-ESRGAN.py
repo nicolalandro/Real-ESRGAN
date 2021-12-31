adf46a4c0bd18888b09cc7e6932b6faf85c30048
nicolalandro <nicolaxx94@Live.it>
2021-12-31 08:50:54 +0100

    empty plugin work into GIMP

---
+++ Real-ESRGAN.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#    This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+gi.require_version('Gimp', '3.0')
+from gi.repository import Gimp
+gi.require_version('GimpUi', '3.0')
+from gi.repository import GimpUi
+gi.require_version('Gegl', '0.4')
+from gi.repository import Gegl
+from gi.repository import GObject
+from gi.repository import GLib
+from gi.repository import Gio
+
+import gettext
+import os
+import sys
+from inference_realesrgan import apply_real_esr_gan
+
+textdomain = 'gimp30-std-plug-ins'
+gettext.bindtextdomain(textdomain, Gimp.locale_directory())
+#gettext.bind_textdomain_codeset(textdomain, 'UTF-8')
+gettext.textdomain(textdomain)
+_ = gettext.gettext
+def N_(message): return message
+
+
+class ArgsFromDict:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+def save_image(image, drawable, file_path):
+    interlace, compression = 0, 2
+    Gimp.get_pdb().run_procedure(
+        "file-png-save",
+        [
+            GObject.Value(Gimp.RunMode, Gimp.RunMode.NONINTERACTIVE),
+            GObject.Value(Gimp.Image, image),
+            GObject.Value(GObject.TYPE_INT, 1),
+            GObject.Value(
+                Gimp.ObjectArray, Gimp.ObjectArray.new(Gimp.Drawable, [drawable], 0)
+            ),
+            GObject.Value(
+                Gio.File,
+                Gio.File.new_for_path(file_path),
+            ),
+            GObject.Value(GObject.TYPE_BOOLEAN, interlace),
+            GObject.Value(GObject.TYPE_INT, compression),
+
+            GObject.Value(GObject.TYPE_BOOLEAN, True),
+            GObject.Value(GObject.TYPE_BOOLEAN, True),
+            GObject.Value(GObject.TYPE_BOOLEAN, False),
+            GObject.Value(GObject.TYPE_BOOLEAN, True),
+        ],
+    )
+
+def load_image(file_path):
+    img = Gimp.get_pdb().run_procedure(
+        "gimp-file-load",
+        [
+            GObject.Value(Gimp.RunMode, Gimp.RunMode.NONINTERACTIVE),
+            GObject.Value(
+                Gio.File,
+                Gio.File.new_for_path(file_path),
+            ),
+        ]
+    ).index(1)
+    return img
+
+
+class SaveAndLoad(Gimp.PlugIn):
+    ## GimpPlugIn virtual methods ##
+    def do_query_procedures(self):
+        # Localization for the menu entries. It has to be called in the
+        # query function only.
+        self.set_translation_domain(textdomain,
+                                    Gio.file_new_for_path(Gimp.locale_directory()))
+
+        return [ "plug-in-real-ESRGan-python" ]
+
+    def do_create_procedure(self, name):
+        procedure = Gimp.ImageProcedure.new(self, name,
+                                       Gimp.PDBProcType.PLUGIN,
+                                       self.run, None)
+
+        procedure.set_image_types("*")
+        procedure.set_sensitivity_mask (Gimp.ProcedureSensitivityMask.DRAWABLE)
+
+        procedure.set_menu_label(N_("Real ESRGan"))
+        procedure.set_icon_name(GimpUi.ICON_GEGL)
+        procedure.add_menu_path('<Image>/Filters')
+
+        procedure.set_documentation(N_("Real ESRGan"),
+                                    N_("Deep 4x resolution"),
+                                    name)
+        procedure.set_attribution("z-uo", "z-uo", "2021")
+
+        return procedure
+
+    def run(self, procedure, run_mode, image, n_drawables, drawables, args, run_data):
+        if n_drawables != 1:
+            msg = _("Procedure '{}' only works with one drawable.").format(procedure.get_name())
+            error = GLib.Error.new_literal(Gimp.PlugIn.error_quark(), msg, 0)
+            return procedure.new_return_values(Gimp.PDBStatusType.CALLING_ERROR, error)
+        else:
+            drawable = drawables[0]
+
+        if run_mode == Gimp.RunMode.INTERACTIVE:
+            gi.require_version('Gtk', '3.0')
+            from gi.repository import Gtk
+            gi.require_version('Gdk', '3.0')
+            from gi.repository import Gdk
+
+            GimpUi.init("Real-ESRGan.py")
+
+            dialog = GimpUi.Dialog(use_header_bar=True,
+                                   title=_("Real ESRGan"),
+                                   role="real-ESRGan-Python3")
+
+            dialog.add_button(_("_Cancel"), Gtk.ResponseType.CANCEL)
+            dialog.add_button(_("_OK"), Gtk.ResponseType.OK)
+
+            geometry = Gdk.Geometry()
+            geometry.min_aspect = 0.5
+            geometry.max_aspect = 1.0
+            dialog.set_geometry_hints(None, geometry, Gdk.WindowHints.ASPECT)
+
+            box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+            dialog.get_content_area().add(box)
+            box.show()
+
+            # Label text content
+            label = Gtk.Label(label='Text:')
+            box.pack_start(label, False, False, 1)
+            label.show()
+
+            text_path = Gtk.TextView()
+            buffer = text_path.get_buffer()
+            buffer.set_text('/tmp', -1)
+            box.pack_start(text_path, True, True, 1)
+            text_path.show()
+
+
+            while (True):
+                response = dialog.run()
+                if response == Gtk.ResponseType.OK:
+                    base_dir = buffer.get_text(buffer.get_start_iter(), buffer.get_end_iter(), True)
+
+                    # save
+                    save_image(image, drawable, os.path.join(base_dir, "cache.png"))
+
+                    # compute something and resave image
+
+                    # load and create layer
+                    img = load_image(os.path.join(base_dir, "cache.png"))
+                    result_layer = img.get_active_layer()
+                    layer = Gimp.Layer.new_from_drawable(result_layer, image)
+                    layer.set_name("loaded")
+                    #layer = Gimp.Layer.new(
+                        #img, 'loaded',
+                        #drawable.get_width(), drawable.get_height(),
+                        #Gimp.ImageType.RGBA_IMAGE, 100.0,
+                        #Gimp.LayerMode.NORMAL
+                    #)
+                    position = Gimp.get_pdb().run_procedure('gimp-image-get-item-position',
+                                 [image,
+                                  drawable]).index(1)
+                    image.insert_layer(layer,None,position)
+
+                    # close dialog
+                    dialog.destroy()
+                    break
+                else:
+                    dialog.destroy()
+                    return procedure.new_return_values(Gimp.PDBStatusType.CANCEL,
+                                                       GLib.Error())
+
+
+        return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
+
+Gimp.main(SaveAndLoad.__gtype__, sys.argv)
--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -7,40 +7,7 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
 from realesrgan.archs.srvgg_arch import SRVGGNetCompact
 
-
-def main():
-    """Inference demo for Real-ESRGAN.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', type=str, default='inputs', help='Input image or folder')
-    parser.add_argument(
-        '-n',
-        '--model_name',
-        type=str,
-        default='RealESRGAN_x4plus',
-        help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus'
-              'RealESRGANv2-anime-xsx2 | RealESRGANv2-animevideo-xsx2-nousm | RealESRGANv2-animevideo-xsx2'
-              'RealESRGANv2-anime-xsx4 | RealESRGANv2-animevideo-xsx4-nousm | RealESRGANv2-animevideo-xsx4'))
-    parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
-    parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
-    parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored image')
-    parser.add_argument('-t', '--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
-    parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
-    parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
-    parser.add_argument('--face_enhance', action='store_true', help='Use GFPGAN to enhance face')
-    parser.add_argument('--half', action='store_true', help='Use half precision during inference')
-    parser.add_argument(
-        '--alpha_upsampler',
-        type=str,
-        default='realesrgan',
-        help='The upsampler for the alpha channels. Options: realesrgan | bicubic')
-    parser.add_argument(
-        '--ext',
-        type=str,
-        default='auto',
-        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
-    args = parser.parse_args()
-
+def apply_real_esr_gan(args):
     # determine models according to model names
     args.model_name = args.model_name.split('.')[0]
     if args.model_name in ['RealESRGAN_x4plus', 'RealESRNet_x4plus']:  # x4 RRDBNet model
@@ -122,6 +89,42 @@ def main():
                 extension = 'png'
             save_path = os.path.join(args.output, f'{imgname}_{args.suffix}.{extension}')
             cv2.imwrite(save_path, output)
+
+
+def main():
+    """Inference demo for Real-ESRGAN.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, default='inputs', help='Input image or folder')
+    parser.add_argument(
+        '-n',
+        '--model_name',
+        type=str,
+        default='RealESRGAN_x4plus',
+        help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus'
+              'RealESRGANv2-anime-xsx2 | RealESRGANv2-animevideo-xsx2-nousm | RealESRGANv2-animevideo-xsx2'
+              'RealESRGANv2-anime-xsx4 | RealESRGANv2-animevideo-xsx4-nousm | RealESRGANv2-animevideo-xsx4'))
+    parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
+    parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
+    parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored image')
+    parser.add_argument('-t', '--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
+    parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
+    parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
+    parser.add_argument('--face_enhance', action='store_true', help='Use GFPGAN to enhance face')
+    parser.add_argument('--half', action='store_true', help='Use half precision during inference')
+    parser.add_argument(
+        '--alpha_upsampler',
+        type=str,
+        default='realesrgan',
+        help='The upsampler for the alpha channels. Options: realesrgan | bicubic')
+    parser.add_argument(
+        '--ext',
+        type=str,
+        default='auto',
+        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
+    args = parser.parse_args()
+
+    apply_real_esr_gan(args)
 
 
 if __name__ == '__main__':
